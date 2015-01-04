598ed61c0af6f02ec10dcfda0680bf63c7d8790a
Arnav Gupta <championswimmer@gmail.com>
2015-01-04 19:27:34 +0530

    Initial commit

---
+++ app/src/main/java/in/tosc/eventful2/MainActivity.java
@@ -0,0 +1,9 @@
+package in.tosc.eventful2;
+
+
+import in.tosc.libeventful2.activities.TopNavdrawerActivity;
+
+
+public class MainActivity extends TopNavdrawerActivity {
+
+}
---
+++ libeventful2/src/main/java/in/tosc/libeventful2/activities/TopExtendableActivity.java
@@ -0,0 +1,10 @@
+package in.tosc.libeventful2.activities;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by championswimmer on 4/1/15.
+ */
+public class TopExtendableActivity extends ActionBarActivity {
+
+}
