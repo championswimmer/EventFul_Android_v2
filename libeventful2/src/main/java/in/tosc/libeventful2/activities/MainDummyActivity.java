bb5299e42278d5a2e0e55c36486f5aafb5b68dcf
Arnav Gupta <championswimmer@gmail.com>
2015-01-12 04:37:09 +0530

    properly do full-screen
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- libeventful2/src/main/java/in/tosc/libeventful2/activities/MainDummyActivity.java
+++ libeventful2/src/main/java/in/tosc/libeventful2/activities/MainDummyActivity.java
@@ -19,10 +19,6 @@ public class MainDummyActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().addFlags(View.SYSTEM_UI_FLAG_FULLSCREEN);
-        getWindow().addFlags(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        getWindow().addFlags(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        getWindow().addFlags(View.SYSTEM_UI_FLAG_IMMERSIVE);
         setContentView(R.layout.activity_main_dummy);
         ImageView splashScreen = (ImageView) findViewById(R.id.splash_screen);
         try {
